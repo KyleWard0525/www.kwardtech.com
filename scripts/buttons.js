75808b6c9e8490a76491c5f6b5db1c25011a2ceb
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-04-27 15:14:07 -0400

    Handles button presses on the site

---
+++ scripts/buttons.js
@@ -0,0 +1,19 @@
+function handleNavTestBtn()
+{
+    //  Get page and create new text node
+    var mainPage = document.getElementById("main-bg-image");
+    var msg = document.createTextNode("The nav button was pressed successfully!");
+
+    //  Style page element
+    mainPage.style.textAlign = "center";
+    mainPage.style.fontWeight = "lighter";
+    mainPage.style.fontStyle = "italic";
+    mainPage.style.fontSize = "24px";
+    mainPage.style.fontFamily = "Times New Roman,Times,serif";
+
+    //  Add message to page
+    mainPage.append(msg);
+
+    //  Remove nav button
+    document.getElementById("nav-btn-test").remove();
+}
