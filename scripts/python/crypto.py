15a7532a68f08e08b25066a58f738705b3524308
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-10-31 13:13:39 -0400

    Cryptography

--- scripts/python/crypto.py
+++ scripts/python/crypto.py
@@ -5,18 +5,22 @@ for various cryptographic functions
 from cryptography.fernet import Fernet
 from lockfile import LockFile
 import os
+import sys
 import time
+import threading
+import random
 
 class Crypto:
 
-    regenTime = 120                 #   Regenerate a new key every 120 seconds
+    regenTime = 10                  #   Regenerate a new key every 120 seconds
     keygenTime = 0                  #   Timepoint at which new key was generated
     keyFilename = "cryptkey.key"    #   Crypto key file name
+    dataEntries = []                #   File location for data that has been encrypted
 
     #   Read encryption key from file. If no file exists, generate a new key
     def readKey(self):
         #   Generate new key
-        if not os.path.exists(self.keyFileName):
+        if not os.path.exists(self.keyFilename):
 
             return self.newKey()
         
@@ -25,7 +29,7 @@ class Crypto:
             #   Open key file and read from file
             keyFile = open(self.keyFilename, "rb")
             key = keyFile.read()
-            return
+            return key
 
     #   Initialize Crypto class
     def __init__(self):
@@ -41,7 +45,7 @@ class Crypto:
         
 
         #   Create file to store key
-        keyFile = open("cryptkey.key", "wb")
+        keyFile = open(self.keyFilename, "wb")
 
         #   Generate encryption key
         key = Fernet.generate_key()
@@ -65,37 +69,84 @@ class Crypto:
         #   Compute elapsed time
         elapsed = end - self.keygenTime
 
-        print("Key age = %.2fs" % elapsed)
-
         #   Check if new key is needed
         if elapsed >= self.regenTime:
-            self.newKey()
-            print("Encryption key has been successfully regenerated!")
-        else:
-            #   Wait for next keygen
-            while elapsed < self.regenTime:
-                elapsed = time.time() - self.keygenTime
+            self.key = self.newKey()
             
-            #   Generate new key with recursion
-            self.checkKeyAge()
+            #   Re-encrypt data with new key
+            for data in range(len(self.dataEntries)):
+                if type(self.dataEntries[data]) is bytes:
+                    self.dataEntries[data] = self.encrypt(self.dataEntries[data])
+                else:
+                    self.dataEntries[data] = self.encrypt(self.dataEntries[data].encode())
+            
+            print("\nNew encryption key generated: %s" % self.key)
+            print("Data: " + str(self.dataEntries))
 
 
     #   Encrypt data
     def encrypt(self, data):
 
         #   Create fernet object
-        fern = Fernet(self.key)
+        fern = Fernet(self.readKey())
 
-        #   Encrypt and return data
-        return fern.encrypt(data.encode())
+        if type(data) is bytes:
+            return fern.encrypt(data)
+        else:
+            #   Save raw data to be reencrypted upon keygen
+            self.dataEntries.append(data)
+            
+            #   Encrypt and return data
+            return fern.encrypt(data.encode())
+
+        
 
     #   Decrypt data
     def decrypt(self, data):
         #  Create fernet object
-        fern = Fernet(self.key)
+        fern = Fernet(self.readKey())
 
         #   Decrypt and return data
-        fern.decrypt(data).decode()
+        return fern.decrypt(data).decode()
 
 
-crypt = Crypto()
+#   Threaded class for measuring elapsed keygen time
+class KeyGenThread(threading.Thread):
+
+    def __init__(self, threadID, threadName, objCrypto):
+        threading.Thread.__init__(self) #   Initialize threading class
+        self.id = threadID
+        self.name = threadName
+        self.crypto = objCrypto         #   Cryptography object
+        
+
+    #   Wait until next keygen
+    def run(self):
+        while True:
+
+            #   Compute elapsed time
+            elapsed = time.time() - self.crypto.keygenTime
+            
+            #   Wait for next keygen opportunity
+            while elapsed < self.crypto.regenTime:
+                elapsed = time.time() - self.crypto.keygenTime
+
+            #   Check for keygen opportunity
+            self.crypto.checkKeyAge()  
+
+
+
+if __name__ == "__main__":
+    crypt = Crypto()
+    threadKeygen = KeyGenThread(0, "keygen-thread", crypt)
+    threadKeygen.start()
+    crypt.encrypt("hello")
+
+    while True:
+        if random.random() > 0.25:
+            for data in crypt.dataEntries:
+                print("Decrypted data = %s" % crypt.decrypt(data.encode()))
+        else:
+            time.sleep(crypt.regenTime/4)
+
+
