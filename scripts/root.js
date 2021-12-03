1d39733ce66a2efd00c3dba8c4f6f545c026b251
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-12-03 11:35:27 -0500

    Added launchTheGame()

--- scripts/root.js
+++ scripts/root.js
@@ -12,6 +12,21 @@ function onLoad()
         // Fade in content
         $("main").hide();
         
-        $("main").fadeIn(mainFadeDelay); 
+        $("main").delay(50).fadeIn(mainFadeDelay); 
     });
+}
+
+// Launch new challenge. The Game
+function launchTheGame()
+{
+    // Print message to user
+    let conf = window.confirm("You are navigating to my web app: 'The Game'.\n\n This is a fun little puzzle site I made for my web development class.\n" +
+    "The project called for us to create a web app for a 'client' so my idea was to test a potential employee's crtical thinking skills in a stressful environment\n\nI hope you enjoy :)\n");
+
+    // Check if user is ok with navigating to The Game
+    if(conf)
+    {
+        // Launch The Game
+        window.location.href = "../TheGame/index.html";
+    }
 }
