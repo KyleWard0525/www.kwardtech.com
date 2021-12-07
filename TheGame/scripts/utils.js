021ac2bcbdc63eb679e528bcb9533f8ae0fe277e
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-12-06 19:24:39 -0500

    Added progressBar() and selectFile()

--- TheGame/scripts/utils.js
+++ TheGame/scripts/utils.js
@@ -2,8 +2,52 @@
  * Utilities for supporting the game
  */
 
+
+// Show progress bar
+export function progressBar(max, delay, callback)
+{
+    // Get current progress value
+    let val = $(".progbar").progressbar("value");
+
+    // Increment progress
+    $(".progbar").progressbar("value", val+1);
+
+    // Check if loading is done
+    if(val < max)
+    {
+        // Recursively run the progress bar until its done
+        setTimeout(progressBar, delay, max, delay, callback);
+    }
+    else {
+        // Run callback function when loading is done
+        callback();
+    }
+    
+}
+
 // Generate a random integer in a given range
-function randint(min, max)
+export function randint(min, max)
 {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+// Open file selector dialog
+export function selectFile(callbackFunc)
+{
+    // Create input variable and set type to file
+    var input = document.createElement("input");
+    input.type = 'file';
+
+    // Handle file selection
+    input.onchange = evt => {
+
+        // Get selected file
+        var file = evt.target.files[0];
+
+        // Handle file with callback function
+        callbackFunc(file);
+    }
+
+    input.click();
+}
+
