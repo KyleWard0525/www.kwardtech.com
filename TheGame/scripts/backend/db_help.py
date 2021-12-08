ebb9651d08ed57efd631139c2dbb3cf3929cfb0c
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-12-08 14:10:40 -0500

    Fixed update_user_attr

--- TheGame/scripts/backend/db_help.py
+++ TheGame/scripts/backend/db_help.py
@@ -8,19 +8,32 @@ from backend import mongo
 from bson import ObjectId
 
 # Check if user exists
-def user_exists(data):
-    print(data)
+def user_exists(ip):
 
-    return not (mongo.db.users.find_one({"metadata": data}) == None)
+    return not (mongo.db.users.find_one({"ip": ip}) == None)
 
 # Attempt to add user to the database
 def add_user(user):
     # Check if user already exists
-    if not user_exists(user['metadata']):
-        print("\n\nUSER NOT FOUND\n\n")
-
+    if not user_exists(user['ip']):
         mongo.db.users.insert_one(user)
+        print("\nUSER ADDED\n\n")
         return True
     else:
-        print("\n\nUSER ALREADY EXISTS\n\n")
+        print("\nERROR in add_user(): USER ALREADY EXISTS\n")
         return False
+
+# Update user attribute
+def update_user_attr(user_ip, attr_name, attr_val):
+    if not user_exists(user_ip):
+        print("\nERROR in DataBase.add_user_attr(): User with IP Address '" + str(user_ip) + "' not found")
+        return False
+
+    # Update user with new attribute
+    return mongo.db.users.update_one(
+        {'ip': user_ip},
+            {
+                '$set': {attr_name: attr_val},
+                '$currentDate': { 'lastModified': True}
+            }
+    )
