3ab1d3bb5f6ff1a3c24f5e617ac53d304e09f1cc
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-12-08 15:57:29 -0500

    Added /save_file

--- TheGame/scripts/backend/routes.py
+++ TheGame/scripts/backend/routes.py
@@ -4,7 +4,7 @@ This file contains the app routes for flask
 kward
 """
 import json
-from flask import request                   #   For communicating with the front end
+from flask import request
 from backend import app                     #   Import flask app
 from backend import db_help                 #   DB helper functions
 
@@ -41,10 +41,33 @@ def init_metadata():
     user['metadata'] = data
     user['ip'] = data['ip']
 
+    # Check if user already exists
+    if db_help.user_exists(user["ip"]):
+        return {"result": 0}
+
     # Attempt to add user to the database
     if(db_help.add_user(user)):
         return {"result": 1}
     else:
         return {"result": 0}
 
+# Save file selected by user
+@app.route("/save_file", methods=["POST"])
+def save_file():
+    # Get data pass from front end
+    data = request.get_json(force=True)
 
+    user_ip = data['ip']
+
+    print(f"\nData from frontend\n\tUSER IP: {user_ip}\n\tFILE: {data['file']}")
+    
+    # Ensure user is in the database
+    if db_help.user_exists(user_ip):
+        # Update user's file attributes
+        db_help.update_user_attr(user_ip, 'file', data['file'])
+
+        return {"result": 1}
+    else:
+        print(f"\nERROR in save_file(): User with ip '{user_ip}' not found")
+
+        return {"result": 0}
